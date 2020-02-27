303fa4f5feabf5765acb8386cd7492db268f43db
Andrew Johnson <ajzat25@gmail.com>
2020-02-27 01:34:58 -0500

    timeout

--- client.js
+++ client.js
@@ -17,11 +17,13 @@ function request (hostname, port, path, options, queries) {
       port: port,
       path: reqUrl,
       method: 'GET',
-      timeout: 2000,
     }
-    // most servers use self signed certs, so the option to disable checking for this is enabled
-    if (options.allowSelfSigned) { reqOptions.rejectUnauthorized = false }
-    if (options.timeout) { reqOptions.timeout = options.timeout }
+    var timeout = 5000
+    if (options){
+      // most servers use self signed certs, so the option to disable checking for this is enabled
+      if (options.allowSelfSigned) { reqOptions.rejectUnauthorized = false }
+      if (options.timeout) { reqOptions.timeout = options.timeout }
+    }
     // send the request
     const req = https.request(reqOptions, function(res) {
       var data = ''
@@ -43,6 +45,7 @@ function request (hostname, port, path, options, queries) {
     })
     // reject errors
     req.on('error', function(err){ reject(err) })
+    req.setTimeout(timeout)
     req.end()
   })
 }
