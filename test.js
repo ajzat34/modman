19d05af58f102a383c6be2ce0925368e6f075613
Andrew Johnson <ajzat25@gmail.com>
2020-02-28 11:38:36 -0500

    started using loader function, cleanup

--- test.js
+++ test.js
@@ -1,5 +1,12 @@
+// the purpose of this file is to provide an example usage, and a simple way
+// to test that everything is wokring properly
+// it includes insecure keys and certificate that should be replaced using openssl
+
 const modman = require('./index.js')
 const fs = require('fs')
+
+// load modules
+console.log('\nWARNING: this script uses a the included key and certificate, you should probably create your own\n')
 
 // create server
 var server = modman.server.create(options = {
@@ -7,29 +14,25 @@ var server = modman.server.create(options = {
   cert: fs.readFileSync('cert.pem'),
   tokens: {'secret': true}
 })
-
+// automaticly configure console logging
 server.setupConsoleLogging('modman')
-
-// load modules
-
-console.log('\nWARNING: this script uses a the included key and certificate, you should probably create your own\n')
 
 console.log('[modman]: loading modules...')
 server
-  .load(require('./packages/sysinfo.js'))
-  .load(require('./packages/pm2.js'))
-  .load(require('./packages/services.js').allow(['cron', 'zfs', 'docker']))
-  .load(require('./packages/docker.js').setTimeoutLength(500))
+  .load(modman.getpkg('sysinfo'))
+  .load(modman.getpkg('pm2'))
+  .load(modman.getpkg('services').allow(['cron', 'docker']))
+  .load(modman.getpkg('docker').setTimeoutLength(500))
 
+// start the server
 server.listen()
 
-// test client
+// test client (needs async wrapper)
 async function client(){
   var client = modman.client.create('localhost', 8000, 'secret', {allowSelfSigned: true})
   try {
     var modules = await client.modules()
-    console.log('[client]: available modules')
-    // console.log(modules)
+    console.log('loaded modules from server')
   } catch (err) {
     console.error(err)
   }
@@ -40,8 +43,6 @@ async function client(){
   } catch (err) {
     console.error(err)
   }
-
   console.log('\nif you reach this point without any errors, it looks like everything is working.\n')
-
 }
 client()
